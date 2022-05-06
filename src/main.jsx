0631bfe84bed6b4b82671c1a31fd284d3d7e1e33
yusoffnike01 <ronaldoyusoff@gmail.com>
2022-05-07 01:08:44 +0800

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode='light'
+      <ColorModeScript initialColorMode='light'/>
     <App />
     </ChakraProvider>
   </React.StrictMode>
